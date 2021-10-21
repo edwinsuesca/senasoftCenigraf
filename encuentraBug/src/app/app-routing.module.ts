81a0e3a06d436d2318d5cdc5d2088a7cd8d07d0d
Edwin Suesca <edwinsuesca@vivaldi.net>
2021-10-21 08:24:01 -0500

    Se implementa interfaz gráfica para el juego

--- encuentraBug/src/app/app-routing.module.ts
+++ encuentraBug/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './components/game/game.component';
+import { NewGameComponent } from './components/new-game/new-game.component';
+import { StartComponent } from './components/start/start.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:NewGameComponent},
+  {path: 'newGame', component:NewGameComponent},
+  {path: 'start', component:StartComponent},
+  {path: 'game', component:GameComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- encuentraBug/src/app/app.module.ts
+++ encuentraBug/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './components/game/game.component';
+import { StartComponent } from './components/start/start.component';
+import { NewGameComponent } from './components/new-game/new-game.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    StartComponent,
+    NewGameComponent
   ],
   imports: [
     BrowserModule,
