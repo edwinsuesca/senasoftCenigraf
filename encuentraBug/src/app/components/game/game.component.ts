f5d85477e140354652869b8f4b8c626c931f9b1f
edwinsuesca <edwinsuesca@vivaldi.net>
2021-10-21 14:28:04 -0500

    Se agrega funcionalidad a los botones de pregrunta y acusación

--- encuentraBug/src/app/components/game/game.component.ts
+++ encuentraBug/src/app/components/game/game.component.ts
@@ -35,12 +35,15 @@ export class GameComponent implements OnInit, AfterViewInit {
     {cardType: 'programadores', name: 'Luis'}
   ]
 
-  constructor() {}
+  constructor() {
+  }
 
   woff = new Audio();
   ping = new Audio();
   @ViewChild('notes') public notes!:ElementRef;
+  @ViewChild('arrowContainer') public button!:ElementRef;
   @ViewChild('arrow') public arrow!:ElementRef;
+
   switchNotes = false; //Establece el estado del panel de notas en falso (oculto).
   panelTitle = "Hoja de Notas";
   ngOnInit(): void {
@@ -64,43 +67,55 @@ export class GameComponent implements OnInit, AfterViewInit {
   modePanel(mode:String){
     
     if(mode == "question"){
-      this.panelTitle = "Preguntar"
+      this.panelTitle = "Preguntar";
       this.switchPanel();
     }
 
     else{
       if(mode == "accusation"){
-        this.panelTitle = "Acusar"
+        this.panelTitle = "Acusar";
         this.switchPanel();
       }
 
       else{
-        this.panelTitle = "Hoja de Notas"
+        this.panelTitle = "Hoja de Notas";
         this.switchPanel();
       }
     }
   }
 
+  public checkCard(card:any){
+
+    if(this.panelTitle == "Hoja de Notas"){
+      card.style.backgroundColor = "#fff";
+      card.style.color = "#1b1b33";
+    }
+
+    var status = [
+      {nombre: card.id, disableStatus: true}
+    ]
+    return  status;
+  }
+
   switchPanel(){
+    this.checkCard;
     //Oculta el panel de notas cuando se da clic en la flecha.
     switch(this.switchNotes){
       case false:
         this.notes.nativeElement.style.top = "0"; //Muestra el panel de notas.
-        this.arrow.nativeElement.style.transform = "rotateX(180deg)"; //Rota flecha.
+        this.button.nativeElement.style.transform = "rotateX(180deg)"; //Rota flecha.
+        this.arrow.nativeElement.style.top = "-10px"; //centra en eje Y flecha.
         this.switchNotes = true;
         this.woff.play();
       break;
 
       case true:
-        this.notes.nativeElement.style.top = "-440px"; //Oculta el panel de notas.
-        this.arrow.nativeElement.style.transform = "rotateX(0deg)"; //reinicia rotación de la flecha.
+        this.notes.nativeElement.style.top = "-339px"; //Oculta el panel de notas.
+        this.button.nativeElement.style.transform = "rotateX(0deg)"; //reinicia rotación de la flecha.
+        this.arrow.nativeElement.style.top = "0"; //centra en eje Y flecha.
         this.switchNotes = false;
         this.woff.play();
       break;
     }
-  }
-
-  checkCard(card: String){
-    console.log(card);
   }
 }
