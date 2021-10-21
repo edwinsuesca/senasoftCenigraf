e79619801a660fa3496073e3965ed4754fa1ff0d
johajan <jamartin10@misena.edu.co>
2021-10-21 11:04:53 -0500

    Título dinámico para panel

--- encuentraBug/src/app/components/game/game.component.ts
+++ encuentraBug/src/app/components/game/game.component.ts
@@ -42,7 +42,7 @@ export class GameComponent implements OnInit, AfterViewInit {
   @ViewChild('notes') public notes!:ElementRef;
   @ViewChild('arrow') public arrow!:ElementRef;
   switchNotes = false; //Establece el estado del panel de notas en falso (oculto).
-
+  panelTitle = "Hoja de Notas";
   ngOnInit(): void {
     this.woff.volume = 0.1;
     this.ping.volume = 0.1;
@@ -51,24 +51,6 @@ export class GameComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void{
-    //Oculta el panel de notas cuando se da clic en la flecha.
-    this.arrow.nativeElement.addEventListener('click', ()=>{
-      switch(this.switchNotes){
-        case false:
-          this.notes.nativeElement.style.top = "0"; //Muestra el panel de notas.
-          this.arrow.nativeElement.style.transform = "rotateX(180deg)"; //Rota flecha.
-          this.switchNotes = true;
-          this.woff.play();
-        break;
-
-        case true:
-          this.notes.nativeElement.style.top = "-440px"; //Oculta el panel de notas.
-          this.arrow.nativeElement.style.transform = "rotateX(0deg)"; //reinicia rotación de la flecha.
-          this.switchNotes = false;
-          this.woff.play();
-        break;
-      }
-    })
   }
 
   //arranca la música en bucle
@@ -77,5 +59,48 @@ export class GameComponent implements OnInit, AfterViewInit {
     ping.src = "../../assets/music/ping.mpeg";
     ping.play();
     ping.volume = 0.05;
+  }
+
+  modePanel(mode:String){
+    
+    if(mode == "question"){
+      this.panelTitle = "Preguntar"
+      this.switchPanel();
+    }
+
+    else{
+      if(mode == "accusation"){
+        this.panelTitle = "Acusar"
+        this.switchPanel();
+      }
+
+      else{
+        this.panelTitle = "Hoja de Notas"
+        this.switchPanel();
+      }
+    }
+  }
+
+  switchPanel(){
+    //Oculta el panel de notas cuando se da clic en la flecha.
+    switch(this.switchNotes){
+      case false:
+        this.notes.nativeElement.style.top = "0"; //Muestra el panel de notas.
+        this.arrow.nativeElement.style.transform = "rotateX(180deg)"; //Rota flecha.
+        this.switchNotes = true;
+        this.woff.play();
+      break;
+
+      case true:
+        this.notes.nativeElement.style.top = "-440px"; //Oculta el panel de notas.
+        this.arrow.nativeElement.style.transform = "rotateX(0deg)"; //reinicia rotación de la flecha.
+        this.switchNotes = false;
+        this.woff.play();
+      break;
+    }
+  }
+
+  checkCard(card: String){
+    console.log(card);
   }
 }
