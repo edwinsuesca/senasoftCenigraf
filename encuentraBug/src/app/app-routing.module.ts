cb2cd7f744c03cb4e6acaa3bd53fba2cffe1b4e9
Edwin Suesca <edwinsuesca@vivaldi.net>
2021-10-21 09:27:53 -0500

    Borrada página 'start'. Se dejó ventana para nuevo juego y ventana para ejecución del juego

--- encuentraBug/src/app/app-routing.module.ts
+++ encuentraBug/src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GameComponent } from './components/game/game.component';
 import { NewGameComponent } from './components/new-game/new-game.component';
-import { StartComponent } from './components/start/start.component';
 
 const routes: Routes = [
   {path: '', component:NewGameComponent},
   {path: 'newGame', component:NewGameComponent},
-  {path: 'start', component:StartComponent},
   {path: 'game', component:GameComponent}
 ];
 
--- encuentraBug/src/app/app.module.ts
+++ encuentraBug/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './components/game/game.component';
-import { StartComponent } from './components/start/start.component';
 import { NewGameComponent } from './components/new-game/new-game.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
-    StartComponent,
     NewGameComponent
   ],
   imports: [
