43d3b08529b49490d97898e8975fd50f135f0345
Rafael Chica <rrchica@misena.edu.co>
2021-10-22 12:59:23 -0500

    Se perfeccionan estilos y comunicacion de componentes

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Game } from 'src/app/models/player';
+
 import { GameService } from '../game.service';
 
 @Component({
@@ -10,46 +9,34 @@ import { GameService } from '../game.service';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit, AfterViewInit {
+ //No borrar
   juego!:{codigo:string, nickname: string, programador:string, error:string, modulo:string}
   
-  
+  cartaModulo = "";
+  cartaProgramador = "";
+  cartaError = "";
 
+  routeDev = "";
 
-  player1 = [
-    {cardType: 'programadores', name: 'Luis'},
-    {cardType: 'programadores', name: 'Luis'},
-    {cardType: 'programadores', name: 'Luis'},
-    {cardType: 'programadores', name: 'Luis'}
-  ]
-
-  player2 = [
-    {cardType: 'programadores', name: 'Luis'},
-    {cardType: 'programadores', name: 'Luis'},
-    {cardType: 'programadores', name: 'Luis'},
-    {cardType: 'programadores', name: 'Luis'}
-  ]
-
-  player3 = [
-    {cardType: 'programadores', name: 'Luis'},
-    {cardType: 'programadores', name: 'Luis'},
-    {cardType: 'programadores', name: 'Luis'},
-    {cardType: 'programadores', name: 'Luis'}
-  ]
-
-  player4 = [
-    {cardType: 'programadores', name: 'Luis'},
-    {cardType: 'programadores', name: 'Luis'},
-    {cardType: 'programadores', name: 'Luis'},
-    {cardType: 'programadores', name: 'Luis'}
-  ]
-
+  //No borrar el constructor
   constructor( private route: ActivatedRoute, private gameSvc: GameService) {}
 
   woff = new Audio();
   ping = new Audio();
   @ViewChild('notes') public notes!:ElementRef;
+  @ViewChild('arrowContainer') public button!:ElementRef;
   @ViewChild('arrow') public arrow!:ElementRef;
+  @ViewChild('generateQuestion') public generateQuestion!:ElementRef;
+
+  @ViewChild('cartaModulo') public cardMod!:ElementRef;
+  @ViewChild('cartaProgramador') public cardDev!:ElementRef;
+  @ViewChild('cartaError') public cardErr!:ElementRef;
+
   switchNotes = false; //Establece el estado del panel de notas en falso (oculto).
+  panelTitle = "Hoja de Notas";
+  actionTurn = "";
+  mode = "";
+  prueba: any = "";
 
   ngOnInit(): void {
     this.woff.volume = 0.1;
@@ -57,6 +44,8 @@ export class GameComponent implements OnInit, AfterViewInit {
     this.woff.src = "../../assets/music/uoff.mpeg";
     this.ping.src = "../../assets/music/ping.mpeg";
 
+
+    //No borrar
     this.juego={
       codigo: this.route.snapshot.params.codPartida,
       nickname: this.route.snapshot.params.nickname,
@@ -67,6 +56,12 @@ export class GameComponent implements OnInit, AfterViewInit {
 
     console.log('Programador->', this.juego.programador);
     console.log('Error->', this.juego.error);
+    //
+
+
+    this.cartaProgramador=this.juego.programador;
+    this.cartaModulo=this.juego.codigo;
+    this.cartaError=this.juego.error
      
     
 
@@ -77,26 +72,36 @@ export class GameComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void{
-    //Oculta el panel de notas cuando se da clic en la flecha.
-    this.arrow.nativeElement.addEventListener('click', ()=>{
-      switch(this.switchNotes){
-        case false:
-          this.notes.nativeElement.style.top = "0"; //Muestra el panel de notas.
-          this.arrow.nativeElement.style.transform = "rotateX(180deg)"; //Rota flecha.
-          this.switchNotes = true;
-          this.woff.play();
-        break;
-
-        case true:
-          this.notes.nativeElement.style.top = "-440px"; //Oculta el panel de notas.
-          this.arrow.nativeElement.style.transform = "rotateX(0deg)"; //reinicia rotación de la flecha.
-          this.switchNotes = false;
-          this.woff.play();
-        break;
-      }
-    })
+    this.woff.volume = 0.1;
+    this.ping.volume = 0.1;
+    this.woff.src = "../../assets/music/uoff.mpeg";
+    this.ping.src = "../../assets/music/ping.mpeg";
   }
+  modePanel(mode:String){
+    
+    if(mode == "question"){
+      this.panelTitle = "Preguntar";
+      this.switchPanel();
+      this.generateQuestion.nativeElement.style.opacity = "1";
+      this.actionTurn = "Preguntar Ahora"
+    }
 
+    else{
+      if(mode == "accusation"){
+        this.panelTitle = "Acusar";
+        this.switchPanel();
+        this.generateQuestion.nativeElement.style.opacity = "1";
+        this.actionTurn = "Acusar Ahora"
+      }
+
+      else{
+        this.panelTitle = "Hoja de Notas";
+        this.switchPanel();
+        this.actionTurn = ""
+      }
+    }
+    this.mode = mode.toString();
+  }
   //arranca la música en bucle
   playAudio(){
     let ping = new Audio();
@@ -104,4 +109,50 @@ export class GameComponent implements OnInit, AfterViewInit {
     ping.play();
     ping.volume = 0.05;
   }
+
+  public checkCard(card:any){
+
+    if(this.panelTitle == "Hoja de Notas"){
+      card.style.backgroundColor = "#fff";
+      card.style.color = "#1b1b33";
+    }
+
+    var status = [
+      {nombre: card.id, disableStatus: true}
+    ]
+    return  status; //Retorna estado de carta en hoja de notas.
+    this.prueba = status;
+    console.log(this.prueba[0]);
+  }
+  
+  switchPanel(){
+    this.checkCard;
+    //Oculta el panel de notas cuando se da clic en la flecha.
+    switch(this.switchNotes){
+      case false:
+        this.notes.nativeElement.style.top = "0"; //Muestra el panel de notas.
+        this.button.nativeElement.style.transform = "rotateX(180deg)"; //Rota flecha.
+        this.arrow.nativeElement.style.top = "-10px"; //centra en eje Y flecha.
+        this.switchNotes = true;
+        this.woff.play();
+      break;
+
+      case true:
+        this.notes.nativeElement.style.top = "-320px"; //Oculta el panel de notas.
+        this.button.nativeElement.style.transform = "rotateX(0deg)"; //reinicia rotación de la flecha.
+        this.arrow.nativeElement.style.top = "0"; //centra en eje Y flecha.
+        this.switchNotes = false;
+        this.woff.play();
+      break;
+    }
+  }
+
+  /*const cartasProgramador=["Pedro", "Juan", "Carlos", "Juanita", "Antonio", "Carolina", "Manuel"];
+const cartasModulo=["Nomina", "Facturación", "Recibos", "Comprobantes", "Usuarios", "Contabilidad"];
+const cartasError=["404", "Stack Overflow", "Memory Out of Range", "Null pointer", "Syntax error", "Encoding error"];*/
+  // cardsDinamics(){
+  //   if(this.cartaModulo == "Pedro"){
+  //     this.routeDev = "../../../assets/img/programadores/juan.svg";
+  //   }
+  // }
 }
